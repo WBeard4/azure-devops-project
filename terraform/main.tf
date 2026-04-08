66a6b1b9229f934c4ba9efd85e0a6eac210f7e9c
wbeard4 <wmb6598@gmail.com>
2026-04-08 12:44:50 +0100

    add app service plan, changed location

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,7 +10,11 @@ terraform {
 }
 
 provider "azurerm" {
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
 }
 
 data "azurerm_client_config" "current" {}
@@ -18,6 +22,20 @@ data "azurerm_client_config" "current" {}
 resource "azurerm_resource_group" "main" {
   name     = "rg-${var.project_name}-${var.environment}"
   location = var.location
+
+  tags = {
+    project     = var.project_name
+    environment = var.environment
+    managed_by  = "terraform"
+  }
+}
+
+resource "azurerm_service_plan" "main" {
+  name                = "asp-${var.project_name}-${var.environment}"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  os_type             = "Linux"
+  sku_name            = "B1"
 
   tags = {
     project     = var.project_name
